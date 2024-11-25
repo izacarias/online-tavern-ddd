449c19d024befccb5041a1e22c43dc9c43f7ba5b
izacarias <i.zacarias@gmail.com>
2024-11-25 18:11:19 +0100

    Creating the NewCustomer Factory

--- aggregate/costumer.go
+++ aggregate/costumer.go
@@ -4,8 +4,15 @@
 package aggregate
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
 	"github.com/izacarias/online-tavern-ddd/entity"
 	"github.com/izacarias/online-tavern-ddd/valueobject"
+)
+
+var (
+	ErrInvalidPerson = errors.New("A customer has to have a valid name")
 )
 
 type Customer struct {
@@ -16,4 +23,25 @@ type Customer struct {
 	products []*entity.Item
 	// Transaction is not a pointer because it does not change
 	transactions []valueobject.Transaction
+}
+
+// This is an ordinary factory design pattern implementation
+// The funcion creates Customers handling internal logic
+// and recives the name as parameter
+// Here is only validates that name is not empty
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
 }
